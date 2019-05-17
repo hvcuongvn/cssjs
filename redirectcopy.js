2305cebd5ab76b92c9dc0fdd3081f4233a952244
hvcuongvn <50739993+hvcuongvn@users.noreply.github.com>
2019-05-17 21:40:15 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>Muốn copy thì vào website : <a href='https://homelandsg.vn//'>https://homelandsg.vn//"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>Muốn copy thì vào website : <a href='https://homelandsg.vn/'>https:/homelandsg.vn/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
